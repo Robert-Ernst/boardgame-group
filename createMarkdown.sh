9f6188a20006859a2dba9fe0be7e501ef4fd5281
Ernst <9050259+Robert-Ernst@users.noreply.github.com>
2021-12-10 21:54:35 +0700

    Fix round template and small mistakes

--- createMarkdown.sh
+++ createMarkdown.sh
@@ -2,8 +2,10 @@ for file in ./Rounds/* ; do
     # $file == ./Rounds/2021-11-27.md
     nosuffixfile=$(basename -s .md $file)
     # $nosuffixfile == 2021-11-27
+    if [ ! -f "./html/Rounds/$nosuffixfile.html" ]; then
     marked -i $file -o ./html/Rounds/$nosuffixfile.tmp
     sed -e "/replacemeforroundscontent/{r ./html/Rounds/$nosuffixfile.tmp" -e "d}" ./html/roundsTemplate.html > ./html/Rounds/$nosuffixfile.html
     rm ./html/Rounds/$nosuffixfile.tmp
+    fi
 done
 
